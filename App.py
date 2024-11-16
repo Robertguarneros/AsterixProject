bd9b6f9029bd4ee3af0fbb20ffc125ae1c69aeda
Roberto <roberto.guarneros11@gmail.com>
2024-11-16 18:42:53 +0100

    distance function

--- App.py
+++ App.py
@@ -1057,6 +1057,73 @@ def geocentric_to_geodesic(c):
     return np.degrees(lat), np.degrees(lon), alt
 
 
+def get_rotation_matrix(lat, lon):
+    lat_rad = np.radians(lat)
+    lon_rad = np.radians(lon)
+    return np.array([
+        [-np.sin(lon_rad), np.cos(lon_rad), 0],
+        [-np.sin(lat_rad) * np.cos(lon_rad), -np.sin(lat_rad) * np.sin(lon_rad), np.cos(lat_rad)],
+        [np.cos(lat_rad) * np.cos(lon_rad), np.cos(lat_rad) * np.sin(lon_rad), np.sin(lat_rad)]
+    ])
+
+def get_translation_vector(lat, lon, alt):
+    lat_rad = np.radians(lat)
+    lon_rad = np.radians(lon)
+    nu = A / np.sqrt(1 - E2 * np.sin(lat_rad)**2)
+    return np.array([
+        [(nu + alt) * np.cos(lat_rad) * np.cos(lon_rad)],
+        [(nu + alt) * np.cos(lat_rad) * np.sin(lon_rad)],
+        [(nu * (1 - E2) + alt) * np.sin(lat_rad)]
+    ])
+
+def geocentric2system_cartesian(geocentric_coords):
+    geo = {'X': geocentric_coords[0], 'Y': geocentric_coords[1], 'Z': geocentric_coords[2]}  
+    center = {'Lat': 41.10904, 'Lon': 1.226947, 'Alt': 3438.954}
+    R = get_rotation_matrix(center['Lat'], center['Lon'])
+    T = get_translation_vector(center['Lat'], center['Lon'], center['Alt'])
+    
+    input_vector = np.array([[geo['X']], [geo['Y']], [geo['Z']]])
+    result_vector = R @ (input_vector - T)
+    
+    return {
+        'X': result_vector[0, 0],
+        'Y': result_vector[1, 0],
+        'Z': result_vector[2, 0]
+    }
+
+def system_cartesian2system_stereographical(c):
+    class CoordinatesUVH:
+        def __init__(self):
+            self.U = 0
+            self.V = 0
+            self.Height = 0
+
+    res = CoordinatesUVH()
+    center = {'Lat': 41.10904, 'Lon': 1.226947, 'Alt': 3438.954}
+    
+    lat_rad = np.radians(center['Lat'])
+    
+    R_S = (A * (1.0 - E2)) / (1 - E2 * np.sin(lat_rad)**2)**1.5
+    
+    d_xy2 = c['X']**2 + c['Y']**2
+    res.Height = np.sqrt(d_xy2 + (c['Z'] + center['Alt'] + R_S)**2) - R_S
+    
+    k = (2 * R_S) / (2 * R_S + center['Alt'] + c['Z'] + res.Height)
+    res.U = k * c['X']
+    res.V = k * c['Y']
+
+    return {
+        'U': res.U,
+        'V': res.V,
+        'Height': res.Height
+    }
+
+
+def calculate_distance(U1, V1, U2, V2):
+    distance = np.sqrt((U1 - U2)**2 + (V1 - V2)**2) / 1852
+    return distance
+
+
 # Function that processes all the data items in a single line
 def convert_to_csv(input_file, output_file, progress_dialog):
     lines = read_and_split_binary(input_file)
@@ -1389,6 +1456,7 @@ def convert_to_csv(input_file, output_file, progress_dialog):
                     )  # Rho in meters, theta in radians, elevation in radians
 
                     cartesian_coords = polar_to_cartesian(*polar_coords)
+                    print(cartesian_coords)
                     geocentric_coords = cartesian_to_geocentric(
                         cartesian_coords, radar_coords
                     )
